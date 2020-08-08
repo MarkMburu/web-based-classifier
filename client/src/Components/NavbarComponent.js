b780dd104aa4cfdc56ab1340f65154062757825e
MarkMburu <markmburu685@gmail.com>
2020-08-09 02:37:52 +0300

    bug fixes

--- client/src/Components/NavbarComponent.js
+++ client/src/Components/NavbarComponent.js
@@ -1,26 +1,8 @@
 import React from 'react'
 
 const NavbarComponent = () => {
-  const myFunction =()=>{
-    let x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
-  return (
-    <div className="topnav" id="myTopnav">
-    <a href="#home" className="active">Home</a>
-    <a href="#news">News</a>
-    <a href="#contact">Contact</a>
-    <a href="#about">About</a>
-    <a className="icon" onClick={myFunction}>
-      <i className="fa fa-bars"></i>
-    </a>
-  </div>
-  
- 
+   return (
+  <div></div> 
   )
 }
 
